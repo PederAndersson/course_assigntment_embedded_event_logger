a9f580ac3e0d80c83847616a48a1a73f58f50688
Peder Andersson <peder.andersson@live.se>
2026-01-19 18:18:46 +0100

    started working on the sorting algorithms

---
+++ header/sortAlgo.h
@@ -0,0 +1,9 @@
+#ifndef COURSE_ASSIGNTMENT_SORTALGO_H
+#define COURSE_ASSIGNTMENT_SORTALGO_H
+#include "EventLog.h"
+void insertionSort(EventLog* log);
+
+void merge(EventLog* log);
+void mergeSort(EventLog*log);
+
+#endif //COURSE_ASSIGNTMENT_SORTALGO_H
