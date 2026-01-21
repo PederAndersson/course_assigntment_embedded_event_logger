b5f46d0f0f3033f9a29f12c8adeebac7e3f7d02a
Peder Andersson <peder.andersson@live.se>
2026-01-21 17:45:41 +0100

    add insertionsort and mergesort

--- header/sortAlgo.h
+++ header/sortAlgo.h
@@ -1,9 +1,11 @@
 #ifndef COURSE_ASSIGNTMENT_SORTALGO_H
 #define COURSE_ASSIGNTMENT_SORTALGO_H
 #include "EventLog.h"
-void insertionSort(EventLog* log);
 
-void merge(EventLog* log);
-void mergeSort(EventLog*log);
+
+Event** insertionSort(EventLog* log);
+
+Event** mergeSort(EventLog*log);
+
 
 #endif //COURSE_ASSIGNTMENT_SORTALGO_H
